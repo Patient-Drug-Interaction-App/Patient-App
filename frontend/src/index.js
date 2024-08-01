801063b674a88161b481ddc90d94a1c12d8ba7b7
Lea Iodice <liodice@deloitte.com>
2024-08-01 11:05:50 -0400

    updated css for patient.html

--- frontend/src/index.js
+++ frontend/src/index.js
@@ -1,15 +1,15 @@
-import React from 'react';
-import ReactDOM from 'react-dom/client';
-import '../public/static/index.css';
-import App from './App';
+// import React from 'react';
+// import ReactDOM from 'react-dom/client';
+// import '../public/static/index.css';
+// import App from './App';
 
 
-// Create a root.
-const root = ReactDOM.createRoot(document.getElementById('root'));
+// // Create a root.
+// const root = ReactDOM.createRoot(document.getElementById('root'));
 
-// Initial render: Render the App component to the root.
-root.render(
-  <React.StrictMode>
-      <App />
-  </React.StrictMode>
-);
+// // Initial render: Render the App component to the root.
+// root.render(
+//   <React.StrictMode>
+//       <App />
+//   </React.StrictMode>
+// );
